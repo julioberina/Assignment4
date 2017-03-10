542af16bbb19ea814fb7629aecfa35bb7a8b8d88
Julio Berina <julioberina@gmail.com>
2017-03-10 08:31:39 -0800

    Implemented constructor for Animal class

--- src/edu/cpp/cs/cs141/prog_assgmnt_4/Animal.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_4/Animal.java
@@ -11,6 +11,7 @@ package edu.cpp.cs.cs141.prog_assgmnt_4;
  */
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public abstract class Animal {
     
@@ -20,4 +21,14 @@ public abstract class Animal {
     private List<Disease> diseases;
     private List<Vaccine> vaccinations;
     private List<Appointment> appointments;
+    
+    public Animal(Owner owner, String name, int age)
+    {
+        this.owner = owner;
+        this.name = name;
+        this.age = age;
+        diseases = new ArrayList<Disease>();
+        vaccinations = new ArrayList<Vaccine>();
+        appointments = new ArrayList<Appointment>();
+    }
 }
--- src/edu/cpp/cs/cs141/prog_assgmnt_4/Database.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_4/Database.java
@@ -11,7 +11,6 @@ package edu.cpp.cs.cs141.prog_assgmnt_4;
  */
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.io.FileOutputStream;
