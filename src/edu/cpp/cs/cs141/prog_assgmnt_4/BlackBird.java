b7e02a6142f556a2fd8b28a167c493c6c38f0057
Julio Berina <julioberina@gmail.com>
2017-03-10 08:48:04 -0800

    Added subclass colors for Bird

---
+++ src/edu/cpp/cs/cs141/prog_assgmnt_4/BlackBird.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cpp.cs.cs141.prog_assgmnt_4;
+
+/**
+ *
+ * @author jmb
+ */
+public class BlackBird extends Bird {
+    
+    public BlackBird(Owner owner, String name, int age)
+    {
+        super(owner, name, age);
+    }
+}
